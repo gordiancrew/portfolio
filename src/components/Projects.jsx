dc29b2c7e5936e1d853c0c32de4d7008ace91305
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-08-27 18:13:57 +0300

    feat: start projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,21 @@
 import Header from "./utils/Header";
+import "./Projects.css";
 
 function Projects() {
   return (
     <>
       <Header />
-      This is Projects
+      <div className="projects">
+        <div className="project">
+          <div className="project-frame"></div>
+          <div className="project-info"></div>
+        </div>
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+        <div className="project"></div>
+      </div>
     </>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,14 +9,14 @@ function Skills() {
       <Footer />
       <div className="content">
         <div className="content-box content-box-down box-list">
-          <h3 style={{ color: "yellow" }}>Образование:</h3>
+          <h3 style={{ color: 'rgb(254, 155, 98) '}}>Образование:</h3>
           <h4>⬥БГУФК 2006-2011</h4>
           <h4>⬥Epam UpSkill lab Java development 2020-2021</h4>
           <h4>⬥RS School JS Front-end development 2022</h4>
           <h4>⬥RS School React 2023</h4>
         </div>
         <div className="content-box content-box-down box-list">
-          <h3 style={{ color: "yellow" }}>Стек технологий:</h3>
+          <h3 style={{ color: 'rgb(254, 155, 98) ' }}>Стек технологий:</h3>
           <h4>⬥JS, TS, HTML, CSS</h4>
           <h4>⬥React, React Native</h4>
           <h4>⬥Java, Spring framework, SQL</h4>

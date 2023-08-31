3bef31d201ce49d59c853f5541ee16cbf7398ec8
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-08-31 23:51:41 +0300

    add desc project

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -2,20 +2,48 @@ import Footer from "./utils/Footer";
 import Header from "./utils/Header";
 import "./Info.css";
 import { projectData } from "../data/ProjectData";
+import { Link } from "react-router-dom";
 
 function ProjectInfo(props) {
   return (
     <>
       <Header />
       <Footer />
+      <h2>
+        <span style={{ color: "orange" }}>
+          {projectData[props.project].type}{" "}
+        </span>
+        {projectData[props.project].name}
+      </h2>
       <div className="content">
         <div className="box-image">
           <img className="image" src={projectData[props.project].img} />
         </div>
-        <div className="box-description"></div>
-        
+        <div className="box-description">
+          <div className="desc-info">{projectData[props.project].desc}</div>
+        </div>
       </div>
-      <div className="box-info"><div>jjhjhjhhjh</div> <div>jjhjhjhhjh</div></div>
+      <div className="box-info">
+        <span style={{ color: "orange" }}>Deploy: </span>
+        <Link to={projectData[props.project].deploy}>
+          <span style={{ textDecoration: "underline" }}>
+            {projectData[props.project].deploy}
+          </span>
+        </Link>
+      </div>
+      <div className="box-info">
+        <span style={{ color: "orange" }}>GitHub: </span>
+        <span style={{ textDecoration: "underline" }}>
+          {projectData[props.project].github}
+        </span>
+      </div>
+      <div className="box-info">
+        <div>
+          <span style={{ color: "orange" }}>{'Technology: '}</span>
+          {projectData[props.project].technology}
+        </div>
+      </div>
+      <div className="box-info"></div>
     </>
   );
 }
--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -5,12 +5,18 @@ export const projectData = [
   {
     type: "logic game",
     name: " Who wants to be a programmer?",
+    technology:"React, TypeScript, routing, internacionalization, authorization, LocalStorage, adaptive, sound, synthez voice ",
+    github:"https://github.com/gordiancrew/rs-millioner/pull/69",
+    deploy:"https://rs-school-millioner.netlify.app/",
     img: mill,
   },
   {
-    type: "pet project",
+    type: "study project",
     name: "online-store",
+    technology:"Webpack, TypeScript, Team work, routing, SPA, classes, OOP, api",
     github: "https://github.com/gordiancrew/online-store/pull/29",
+    deploy:"https://online-s-t-o-r-e.netlify.app/",
     img: store,
+    desc:"Учебный проект, онлайн магазин. Выполнен в командной работе из 2 человек. Использовано взаимодействие с back end. Есть фильтры товаров, корзина заказов, иммитация оплаты. "
   },
 ];
